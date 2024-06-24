ae7db02026bdfa09b3c8846d291891ee7ab7e89d
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-06-24 14:12:02 -0400

    navbar final setup + navlinks component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
+import Navlinks from "./Navlinks";
 const Navbar = () => {
   return (
     <nav className="bg-base-200">
@@ -20,14 +21,16 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
             >
-              nav links
+              <Navlinks />
             </ul>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal">nav links</ul>
+          <ul className="menu menu-horizontal">
+            <Navlinks />
+          </ul>
         </div>
         <div className="navbar-end">
           {/* THEME */}
