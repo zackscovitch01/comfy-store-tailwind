cb20eb1ea53d8c018f66881a52ee0b2840f6d639
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-06-24 13:53:25 -0400

    navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,8 @@ const Header = () => {
   return (
     <header className="bg-neutral py-2 text-neutral-content">
       <div className="align-element flex justify-center sm:justify-end">
+        {/* USER */}
+        {/* LINKS */}
         <div className="flex gap-x-6 justify-center items-center">
           <Link to="/login" className="link link-hover text-xs sm:text-sm">
             Sign in / Guest
