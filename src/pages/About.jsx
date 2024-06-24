e2c9e74e04b49539f3ae7fb67fc1bab2e297e71f
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-06-24 14:42:12 -0400

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,27 @@
 const About = () => {
-  return <h1 className="text-4xl">About</h1>;
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum pariatur
+        voluptatum beatae perspiciatis veniam id. Commodi dolorum a nihil error
+        quae nulla quis ad, officiis rerum porro distinctio, quia nemo. Lorem
+        ipsum dolor sit amet, consectetur adipisicing elit. Nemo vel similique
+        non quae! Culpa officia ut odio praesentium autem optio, sequi dolor
+        alias aut ullam quia? Earum hic deserunt doloremque!
+      </p>
+    </>
+  );
 };
 export default About;
