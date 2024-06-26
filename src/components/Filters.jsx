a2285aac8fe241cb56f737fe9114c3c0e7274c69
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-06-26 06:04:24 -0400

    shipping filter + price range

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,11 @@
 import { useLoaderData, Form, Link } from "react-router-dom";
 import FormInput from "./FormInput";
+import FormSelect from "./FormSelect";
+import FormRange from "./FormRange";
+import FormCheckbox from "./FormCheckbox";
 
 const Filters = () => {
+  const { meta } = useLoaderData();
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEARCH */}
@@ -11,13 +15,41 @@ const Filters = () => {
         name="search"
         size="input-sm"
       />
+      {/* CATEGORIES */}
+      <FormSelect
+        label="select category"
+        name="category"
+        list={meta.categories}
+        size="select-sm"
+      />
+
+      {/* COMPANIES */}
+      <FormSelect
+        label="select company"
+        name="company"
+        list={meta.companies}
+        size="select-sm"
+      />
+      {/* ORDER */}
+      <FormSelect
+        label="sort by"
+        name="order"
+        list={["A-Z", "Z-A", "Price: High to low", "Price: Low to high"]}
+        size="select-sm"
+      />
+      {/* PRICE */}
+      <FormRange name="price" label="Selected price" size="range-sm" />
+
+      {/* SHIPPING */}
+      <FormCheckbox name="shipping" label="Free Shipping" size="checkbox-sm" />
       {/* BUTTONS */}
-      <button type="submit" className="btn btn-primary btn-sm uppercase">
-        Search
-      </button>
+
       <Link to="/products" className="btn btn-accent btn-sm uppercase">
         Reset
       </Link>
+      <button type="submit" className="btn btn-primary btn-sm uppercase">
+        Search
+      </button>
     </Form>
   );
 };
